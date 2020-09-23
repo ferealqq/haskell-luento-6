8d941dcac97ffb93c8167eb3b0ac23be8e093eab
“ferealqq” <pekka.mattinen@outlook.com>
2020-09-23 14:44:49 +0300

    luento8

---
+++ luento8/src/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+-- stack repl --package containers
+-- data Suhde = Blokattu | Seurattau deriving (Show)
+-- import qualified Data.Map as Map
+-- type Tunnus = Text
+-- "Kissa" :: Tunnus 
+-- data Käyttää = Käyttäjä {suhde :: Map Tunnus Suhde} deriving Show
+-- esimK = Käyttäjä {suhde :: Map Tunnus Suhde} deriving show
+-- esimK = Käyttäjä (Map.fromList [("pekka",Blokattu),("jukka",Seurattu)])
+
+main :: IO ()
+main = do
+  putStrLn "Hello TIEA341"
